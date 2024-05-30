ddff431646d350112214a4c2816dbb5f4c954eb9
Soham Pagi <paguisoham36@gmail.com>
2024-05-30 23:31:21 +0530

    Add gh workflows

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/zustand-demo/",
   plugins: [
     react(),
     VitePWA({
